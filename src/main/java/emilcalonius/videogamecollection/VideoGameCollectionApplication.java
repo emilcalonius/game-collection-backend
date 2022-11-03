67e67d90a3221d7e7f4c6faa0dc249d3a40ee81a
Emil Calonius <emil.calonius@fi.experis.com>
2022-11-03 15:14:06 +0200

    build: initialize spring boot application

---
+++ src/main/java/emilcalonius/videogamecollection/VideoGameCollectionApplication.java
@@ -0,0 +1,13 @@
+package emilcalonius.videogamecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameCollectionApplication.class, args);
+	}
+
+}
