8834b0e464c51af74761c65239de7fcaea8a6ab6
Emil Calonius <emil.calonius@fi.experis.com>
2022-11-04 20:39:00 +0200

    feat: controllers for user and game

--- src/main/java/emilcalonius/videogamecollection/controllers/GameController.java
+++ src/main/java/emilcalonius/videogamecollection/controllers/GameController.java
@@ -1,2 +1,25 @@
-package emilcalonius.videogamecollection.controllers;public class GameController {
+package emilcalonius.videogamecollection.controllers;
+
+import emilcalonius.videogamecollection.models.Game;
+import emilcalonius.videogamecollection.services.GameService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping(path = "api/game")
+public class GameController {
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping
+    ResponseEntity<Collection<Game>> getAllGamesForUser() {
+        return ResponseEntity.ok(gameService.findAllByUser(1));
+    }
 }
--- src/main/java/emilcalonius/videogamecollection/controllers/UserController.java
+++ src/main/java/emilcalonius/videogamecollection/controllers/UserController.java
@@ -1,2 +1,32 @@
-package emilcalonius.videogamecollection.controllers;public class UserController {
+package emilcalonius.videogamecollection.controllers;
+
+import emilcalonius.videogamecollection.mappers.UserMapper;
+import emilcalonius.videogamecollection.models.User;
+import emilcalonius.videogamecollection.models.UserGETDTO;
+import emilcalonius.videogamecollection.models.UserPOSTDTO;
+import emilcalonius.videogamecollection.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path = "api/user")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<User> getCurrentUser() {
+        // TODO
+        return ResponseEntity.ok(userService.findById(1));
+    }
+
+    // TODO user search
 }
