37c9e564f8cae328b47c6b10c14ad1f100c01549
Emil Calonius <emil.calonius@tuni.fi>
2022-11-16 17:45:41 +0200

    feat: get all games for user

--- src/main/java/emilcalonius/videogamecollection/controllers/GameController.java
+++ src/main/java/emilcalonius/videogamecollection/controllers/GameController.java
@@ -3,7 +3,12 @@ package emilcalonius.videogamecollection.controllers;
 import emilcalonius.videogamecollection.mappers.GameMapper;
 import emilcalonius.videogamecollection.models.Game;
 import emilcalonius.videogamecollection.models.GameDTO;
+import emilcalonius.videogamecollection.models.User;
+import emilcalonius.videogamecollection.security.JWTUtil;
 import emilcalonius.videogamecollection.services.GameService;
+import emilcalonius.videogamecollection.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,19 +20,26 @@ import java.util.Collection;
 @RequestMapping(path = "api/game")
 public class GameController {
     private final GameService gameService;
-
     private final GameMapper gameMapper;
+    private final UserService userService;
+    @Autowired
+    private JWTUtil jwtUtil;
 
-    public GameController(GameService gameService, GameMapper gameMapper) {
+    public GameController(GameService gameService, GameMapper gameMapper, UserService userService) {
         this.gameService = gameService;
         this.gameMapper = gameMapper;
+        this.userService = userService;
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping
-    public ResponseEntity<Collection<Game>> getAllGamesForUser() {
-        return ResponseEntity.ok(gameService.findAllByUser(1));
+    public ResponseEntity<Collection<Game>> getAllGamesForUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
+        String name = jwtUtil.validateTokenAndRetrieveSubject(authorization.split(" ")[1]);
+        User user = userService.findByName(name);
+        return ResponseEntity.ok(gameService.findAllByUser(user.getId()));
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping
     public ResponseEntity addGame(@RequestBody GameDTO gameDTO) {
         Game game = gameService.add(gameMapper.gameDTOToGame(gameDTO));
